25cd8faef3e11242a9c78285dc5b7e1d3601cf99
Naquessab <>
2021-06-07 03:45:39 +0000

    Complete Assignment

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,53 +12,79 @@ let candidateAnswer;
 let questions =["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5+3)/2*10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride","true","40","Trajectory","3"];
 let candidateAnswers = [];
-
+let grade = 0;
+const numQuestions = 5;
+let testStatus;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
  candidateName = input.question("Enter your name: ");
-//
+
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
  //question = input.question("Who was the first American woman in space? ");
+
+for (let i = 0; i < questions.length; i ++){
+ candidateAnswers[i] = input.question(console.log(questions[i]));
+
+console.log(`Your Answer:  ${candidateAnswers[i]}
+Correct Answer: ${correctAnswers[i]}`)
+}
+
+}
+
+ /*
 for (let i = 0; i < questions.length; i++){
    // console.log("\n");
   let answer = input.question(questions[i]);
  
   candidateAnswers.push(answer);
 }
-
-}
-
+//return candidateAnswers;
+//}
+*/
 function gradeQuiz(candidateAnswers) {
- let numCorrect = 0;
+ 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  let grade = 0; 
+for (let i=0; i < 5; i++){
+    if (candidateAnswers[i].toLowerCase() == correctAnswers[i].toLowerCase()){
+      // console.log(grade);
+      grade++
+    }
+  }
 
-for(let i = 0; i < candidateAnswers.length; i++) {
+
+// grade / questions.length * 100
+console.log(`>>> Overall Grade: ${grade / questions.length * 100} (${grade} of 5 responses correct) <<<`);
+  if (grade/questions.length * 100 >= 80){
+    console.log(">>> Status: PASSED <<<")
+  } else {
+    console.log(">>> Status: FAILED <<<")
+  }
+  
+     
+  return grade;
+}
+/*
+let numCorrect = 0;
+
+for(i = 0; i < candidateAnswers.length; i++) {
   if (candidateAnswers[i] === correctAnswers[i] ){
-  //  numCorrect++
+  numCorrect++
   }
   console.log(`${i+1}) ${questions[i]}`);
   console.log(`Your Answer: ${candidateAnswers[i]}`);
   console.log(`Correct Answer: ${correctAnswers[i]}`);
   console.log("\n");
-}
 
-/*
-console.log("Your Answer " + candidateAnswers)
-if (candidateAnswers === correctAnswer) {
-console.log ("You have the correct answer");
-} else {
-console.log("You have entered the wrong answer try again");
+ // let grade;
+ grade = (numCorrect / numQuestions) *  100
+//return grade;
 }
-*/
-  let grade;
-  
-
-  return grade;
-}
+ */
 
 function runProgram() {
   askForName();
@@ -83,4 +109,4 @@ module.exports = {
   candidateAnswers: candidateAnswers,
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
-};
+}
