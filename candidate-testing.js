fc9e09c264cd3fe40ea562f3bfce66c1adc88469
Naquessab <>
2021-06-08 16:12:35 +0000

    Made changes

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,11 +8,11 @@ let candidateName= "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? " ;
 let correctAnswer =  "Sally Ride";
-let candidateAnswer;
-let questions =["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5+3)/2*10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
+let candidateAnswer = '';
+let questions =["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", '(5 + 3)/2 * 10 = ? ', "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride","true","40","Trajectory","3"];
 let candidateAnswers = [];
-let grade = 0;
+
 const numQuestions = 5;
 let testStatus;
 
@@ -46,7 +46,7 @@ for (let i = 0; i < questions.length; i++){
 //}
 */
 function gradeQuiz(candidateAnswers) {
- let numCorrect;
+ let numCorrect = 0;
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
   let grade = 0; 
 for (let i=0; i < 5; i++){
